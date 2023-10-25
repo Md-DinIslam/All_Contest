a2153dbb22c4bd9397127adb19b93d1e3daa9c07
Muhammad Dinislam <108214424+Md-DinIslam@users.noreply.github.com>
2023-10-25 22:09:31 +0600

    Add files via upload

---
+++ Contest/STARTERS 105/Bone_Appetit.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+void solve() {
+    int n, m, x, y;
+    cin >> n >> m >> x >> y;
+    cout << ((n * x) + (m * y));
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    // for (int i = 1; i <= t; ++i) {
+    //     cout << "Case " << i << ": ";
+    //     solve();
+    // }
+}
